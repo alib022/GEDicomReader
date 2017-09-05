6ad29800da9650fbd87a72f06ade7ff36f0ae752
alib022 <ali.bakhshinejad@gmail.com>
2017-09-05 16:02:57 -0500

    debuged the location of functions

--- readGEFlow.py
+++ readGEFlow.py
@@ -2,6 +2,7 @@ import dicom,os, glob, scipy.io, numpy, vtk, sys, datetime, argparse
 from clint.textui import colored
 
 
+
 def readGEFlow(args):
           
     print( colored.green("\nLooking for flow data... \n"))
@@ -137,7 +138,7 @@ def readGEFlow(args):
     
       
     if args.eddycurrent:
-        flowCorrected = eddyCurrentCorrection(UOrg, VOrg, WOrg, args)
+        flowCorrected = eddyCurrentCorrection(UOrg, VOrg, WOrg, args.randomnoise, args.eddythreshold, args.eddyplane)
         
         
     else:
@@ -153,7 +154,7 @@ def readGEFlow(args):
     magSize = magDataTemp.shape
     totalNodes = magSize[0] * magSize[1] * magSize[2]
     if args.vtk and args.mat is False:
-        print(colore.yellow("We will not save any file since you didnt select your preference. (VTK or MAT)"))
+        print(colored.yellow("We will not save any file since you didnt select your preference. (VTK or MAT)"))
     
     if args.vtk:
         saveVTK(magDataTemp, flowCorrected,pixel_spc, totalNodes, args.output)
@@ -167,4 +168,166 @@ def readGEFlow(args):
     
     return RefDs
  #   printReport(outPath, RefDs)
+
+def eddyCurrentCorrection(UOrg, VOrg, WOrg, randNoiseThreshold, eddyCurrentThreshold, eddyOrder):
+
+
+    Ustd = numpy.std(UOrg, axis=3)
+    Vstd = numpy.std(VOrg, axis=3)
+    Wstd = numpy.std(WOrg, axis=3)
+
+
+
+    #### Random noise correction
+
+    #randNoiseThreshold = 60.0
+    noiseMask = numpy.logical_and(numpy.logical_and(Ustd > randNoiseThreshold, Vstd > randNoiseThreshold), Wstd > randNoiseThreshold)
+    #noiseMask[:, :cut.start] = False
+    #noiseMask[:, cut.stop:] = False
+
+    #### Eddy currents correction
+
+    #eddyMask = Ustd < 20  
+    eddyMask = Ustd <   eddyCurrentThreshold
+
     
+    del Ustd, Vstd, Wstd  
+    
+    flowCorrected = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2],3,UOrg.shape[3]])
+    shape = (UOrg.shape[0], UOrg.shape[1])
+       
+    xInit = numpy.linspace(0, shape[0], shape[0])
+    yInit = numpy.linspace(0, shape[1], shape[1])
+    
+    X, Y = numpy.meshgrid(xInit, yInit, sparse=False, indexing='ij')
+    
+    X2=X*X
+    Y2=Y*Y
+    XY=X*Y
+    
+    
+    plainU = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2]])
+    plainV = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2]])
+    plainW = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2]])
+    
+    #flowMean = numpy.mean(flowData, axis=4)
+    UOrgMean = numpy.mean(UOrg, axis=3)
+    VOrgMean = numpy.mean(VOrg, axis=3)
+    WOrgMean = numpy.mean(WOrg, axis=3)
+    
+    if eddyOrder == 1:
+        # best-fit linear plane
+        for iIter in range(plainU.shape[2]):
+            
+            #maskDataTemp = maskData[:,:,iIter ]
+            eddyMaskTemp = eddyMask[:,:,iIter]
+            indEddyMask = numpy.argwhere(eddyMaskTemp.ravel()).ravel()
+            # noiseMaskInd = np.argwhere(noiseMask.ravel()).ravel()
+            
+            
+            BU = UOrgMean[:, :, iIter].ravel()[indEddyMask]
+            BV = VOrgMean[:, :, iIter].ravel()[indEddyMask]
+            BW = WOrgMean[:, :, iIter].ravel()[indEddyMask]
+                    
+            D = numpy.ones((len(indEddyMask), 3))
+            D[:, 0] = X.ravel()[indEddyMask]
+            D[:, 1] = Y.ravel()[indEddyMask]
+            
+       
+            CU,_,_,_ = scipy.linalg.lstsq(D, BU)    # coefficients
+            CV,_,_,_ = scipy.linalg.lstsq(D, BV)    # coefficients
+            CW,_,_,_ = scipy.linalg.lstsq(D, BW)
+        
+            # evaluate it on grid
+            plainU[:,:,iIter] = CU[0]*X + CU[1]*Y + CU[2]
+            plainV[:,:,iIter] = CV[0]*X + CV[1]*Y + CV[2]
+            plainW[:,:,iIter] = CW[0]*X + CW[1]*Y + CW[2]
+            
+            
+        
+    
+    elif eddyOrder == 2:
+        # best-fit quadratic curve
+        
+        for iIter in range(plainU.shape[2]):
+            
+            eddyMaskTemp = eddyMask[:,:,iIter]
+            indEddyMask = numpy.argwhere(eddyMaskTemp.ravel()).ravel()
+            BU = UOrgMean[:, :, iIter].ravel()[indEddyMask]
+            BV = VOrgMean[:, :, iIter].ravel()[indEddyMask]
+            BW = WOrgMean[:, :, iIter].ravel()[indEddyMask]
+                    
+            D = numpy.ones((len(indEddyMask), 6))
+            D[:, 0] = X.ravel()[indEddyMask]
+            D[:, 1] = Y.ravel()[indEddyMask]
+            D[:, 2] = XY.ravel()[indEddyMask]
+            D[:, 3] = X2.ravel()[indEddyMask]
+            D[:, 4] = Y2.ravel()[indEddyMask]
+            
+       
+            CU,_,_,_ = scipy.linalg.lstsq(D, BU)    # coefficients
+            CV,_,_,_ = scipy.linalg.lstsq(D, BV)    # coefficients
+            CW,_,_,_ = scipy.linalg.lstsq(D, BW)
+        
+            # evaluate it on grid
+            plainU[:,:,iIter] = CU[0]*X + CU[1]*Y + CU[2]*XY + CU[3]*X2 + CU[4]*Y2 + CU[5]
+            plainV[:,:,iIter] = CV[0]*X + CV[1]*Y + CV[2]*XY + CV[3]*X2 + CV[4]*Y2 + CV[5]
+            plainW[:,:,iIter] = CW[0]*X + CW[1]*Y + CW[2]*XY + CW[3]*X2 + CW[4]*Y2 + CW[5]
+            
+    
+    for k in range(UOrg.shape[3]):
+        
+        flowCorrected[:, :, :,0, k] = UOrg[:, :, :, k] - plainU
+        flowCorrected[:, :, :,1, k] = VOrg[:, :, :, k] - plainV
+        flowCorrected[:, :, :,2, k] = WOrg[:, :, :, k] - plainW
+    
+    #flowCorrected[noiseMask] = 0
+    print("After Eddy correction")
+    
+    return flowCorrected
+ 
+def saveVTK(magDataTemp, flowCorrected,pixel_spc, totalNodes, outPath):
+        
+        for timeIter in range(0,flowCorrected.shape[4]):
+        
+            #   magData = numpy_support.numpy_to_vtk(num_array=magDataTemp.ravel(order='F'), deep=True, array_type=vtk.VTK_DOUBLE)
+            #   flowDataInVTK = numpy_support.numpy_to_vtk(num_array=flowData[:,:,:,:,timeIter].ravel(order='F'), deep=True, array_type=vtk.VTK_DOUBLE)
+        
+            magDataOut = numpy.reshape(magDataTemp, (totalNodes, 1), order='F')
+        
+            flowDataOut = numpy.reshape(flowCorrected[:,:,:,:,timeIter], (totalNodes, 3), order='F')
+        
+        #        #########################
+        #        # Convert the VTK array to vtkImageData
+        
+            img_vtk = vtk.vtkImageData()
+            img_vtk.SetDimensions(magDataTemp.shape)
+            img_vtk.SetSpacing(pixel_spc)
+            img_vtk.SetOrigin(0,0,0)
+    
+        
+            magArray = vtk.vtkDoubleArray()
+            magArray.SetNumberOfComponents(1)
+            magArray.SetNumberOfTuples(img_vtk.GetNumberOfPoints())
+            magArray.SetName("Magnitude")
+    
+            for i in range(0,totalNodes):
+                magArray.SetValue(i,magDataOut[i])
+        
+            flowArray = vtk.vtkDoubleArray()
+            flowArray.SetNumberOfComponents(3)
+            flowArray.SetNumberOfTuples(img_vtk.GetNumberOfPoints())
+            flowArray.SetName("Velocity")
+            for i in range(0,totalNodes):
+                flowArray.SetTuple3(i,flowDataOut[i,0], flowDataOut[i,1], flowDataOut[i,2])
+        
+            img_vtk.GetPointData().AddArray(magArray)
+            img_vtk.GetPointData().AddArray(flowArray)
+        
+        
+            writer = vtk.vtkXMLImageDataWriter()
+            writer.SetFileName(outPath + '/FlowData_%d.vti' % timeIter)
+            writer.SetInputData(img_vtk)
+            ## This is set so we can see the data in a text editor.
+            writer.SetDataModeToAscii()
+            writer.Write()      
