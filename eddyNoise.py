78c08c38ddac536c0dfa50df75ca973e2f89afb2
Ali Bakhshinejad <ali.bkhahinejad@gmail.com>
2018-03-02 18:15:58 -0600

    working eddy current v1

--- eddyNoise.py
+++ eddyNoise.py
@@ -9,7 +9,7 @@ from rolling_window import rolling_window
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def eddyCurrentCorrection(UOrg, VOrg, WOrg, randNoiseThreshold=1, eddyCurrentThreshold=12, eddyOrder=1, plotBool=0, verbous=0, plotEddyPlane=1):
+def eddyCurrentCorrection(UOrg, VOrg, WOrg, randNoiseThreshold=1, eddyCurrentThreshold=8, eddyOrder=2, plotBool=1, verbous=0, plotEddyPlane=1):
 
 
     USTD = numpy.zeros((UOrg.shape[0],UOrg.shape[1],UOrg.shape[2]))
@@ -49,38 +49,40 @@ def eddyCurrentCorrection(UOrg, VOrg, WOrg, randNoiseThreshold=1, eddyCurrentThr
     staticTissueV = VOrg[:,:,:,-1].copy()
     staticTissueW = WOrg[:,:,:,-1].copy()
 
-    staticTissueU[(USTD < (eddyCurrentThreshold*USTD.max()/100)) & (VSTD < (eddyCurrentThreshold*VSTD.max()/100) ) & (WSTD < (eddyCurrentThreshold*WSTD.max()/100))] = 0
-    staticTissueV[(USTD < (eddyCurrentThreshold*USTD.max()/100)) & (VSTD < (eddyCurrentThreshold*VSTD.max()/100) ) & (WSTD < (eddyCurrentThreshold*WSTD.max()/100))] = 0
-    staticTissueW[(USTD < (eddyCurrentThreshold*USTD.max()/100)) & (VSTD < (eddyCurrentThreshold*VSTD.max()/100) ) & (WSTD < (eddyCurrentThreshold*WSTD.max()/100))] = 0
+    staticTissueU[(USTD > (eddyCurrentThreshold*USTD.max()/100)) & (VSTD > (eddyCurrentThreshold*VSTD.max()/100) ) & (WSTD > (eddyCurrentThreshold*WSTD.max()/100))] = 0
+    staticTissueV[(USTD > (eddyCurrentThreshold*USTD.max()/100)) & (VSTD > (eddyCurrentThreshold*VSTD.max()/100) ) & (WSTD > (eddyCurrentThreshold*WSTD.max()/100))] = 0
+    staticTissueW[(USTD > (eddyCurrentThreshold*USTD.max()/100)) & (VSTD > (eddyCurrentThreshold*VSTD.max()/100) ) & (WSTD > (eddyCurrentThreshold*WSTD.max()/100))] = 0
 
     print(staticTissueU.shape)
     
-    if plotBool:    
+    if plotBool:
+        i = 20
 
-        vmax = numpy.max([UOrg[:,:,20,1].max(), staticTissueU[:,:,20].max()])
-        vmin = numpy.min([UOrg[:,:,20,1].min(), staticTissueU[:,:,20].min()])
+        vmax = numpy.max([UOrg[:,:,i,1].max(), staticTissueU[:,:,i].max()])
+        vmin = numpy.min([UOrg[:,:,i,1].min(), staticTissueU[:,:,i].min()])
         vmax = numpy.max([vmax, -vmin])
         vmin = -vmax
     
-        for i in range(1,72):
+        #for i in range(1,3):
         # plot with various axes scales
-            plt.figure(i)
+        plt.figure(i)
 
        
-            plt.subplot(121)
-            plt.imshow(UOrg[:,:,i,1], vmin=vmin, vmax=vmax, cmap='seismic')
-            plt.title('Org data')
+        plt.subplot(121)
+        plt.imshow(UOrg[:,:,i,1], vmin=vmin, vmax=vmax, cmap='seismic')
+        plt.title('Org data')
 
 
            
-            plt.subplot(122)
-            plt.imshow(UOrgtest[:,:,i], vmin=vmin, vmax=vmax, cmap='seismic')
-            plt.title('static tissue')
+        plt.subplot(122)
+        plt.imshow(staticTissueU[:,:,i], vmin=vmin, vmax=vmax, cmap='seismic')
+        plt.title('static tissue')
 
-            plt.show()
+        plt.show()
             
 
-    del USTD, VSTD, WSTD  
+    del USTD, VSTD, WSTD
+
     
     flowCorrected = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2], 3, UOrg.shape[3]])
    
@@ -90,64 +92,70 @@ def eddyCurrentCorrection(UOrg, VOrg, WOrg, randNoiseThreshold=1, eddyCurrentThr
     
     X, Y = numpy.meshgrid(xInit, yInit, sparse=False, indexing='ij')
     
+    
     X2=X*X
     Y2=Y*Y
     XY=X*Y
     
     
-    plainU = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2], UOrg.shape[3]])
-    plainV = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2], UOrg.shape[3]])
-    plainW = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2], UOrg.shape[3]])
+    plainU = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2]])
+    plainV = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2]])
+    plainW = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2]])
     
     if eddyOrder == 1:
         # best-fit linear plane
-        #for kIter in range(UOrg.shape[3]):
         for iIter in range(UOrg.shape[2]):
             
-                BU = staticTissueU[:, :, iIter].ravel()
-                BV = staticTissueV[:, :, iIter].ravel()
-                BW = staticTissueW[:, :, iIter].ravel()
-                    
-                D = numpy.ones((staticTissueU.shape[0]*staticTissueU.shape[1], 3))
-                print(D.shape)
-                D[:, 0] = X.ravel()[BU>0]
-                D[:, 1] = Y.ravel()[BU>0]
+                BUInd = staticTissueU[:,:,iIter].copy()
+                BVInd = staticTissueV[:,:,iIter].copy()
+                BWInd = staticTissueW[:,:,iIter].copy()
+
+                notZeroIndU = numpy.nonzero(BUInd)
+                notZeroIndV = numpy.nonzero(BVInd)
+                notZeroIndW = numpy.nonZero(BWInd)
+
+                BU = BUInd[notZeroIndU].ravel()
+                BV = BVInd[notZeroIndV].ravel()
+                BW = BWInd[notZeroIndW].ravel()
+                
+                DU = numpy.c_[X[notZeroIndU].ravel(), Y[notZeroIndU].ravel(), numpy.ones(len(X[notZeroIndU].ravel()))]
+                DV = numpy.c_[X[notZeroIndV].ravel(), Y[notZeroIndV].ravel(), numpy.ones(len(X[notZeroIndV].ravel()))]
+                DW = numpy.c_[X[notZeroIndV].ravel(), Y[notZeroIndV].ravel(), numpy.ones(len(X[notZeroIndV].ravel()))]
             
-       
-                CU,_,_,_ = scipy.linalg.lstsq(D, BU)    # coefficients
-                CV,_,_,_ = scipy.linalg.lstsq(D, BV)    # coefficients
-                CW,_,_,_ = scipy.linalg.lstsq(D, BW)
+                CU,_,_,_ = scipy.linalg.lstsq(DU, BU)    # coefficients
+                CV,_,_,_ = scipy.linalg.lstsq(DV, BV)    # coefficients
+                CW,_,_,_ = scipy.linalg.lstsq(DW, BW)
         
                 # evaluate it on grid
                 plainU[:,:,iIter] = CU[0]*X + CU[1]*Y + CU[2]
                 plainV[:,:,iIter] = CV[0]*X + CV[1]*Y + CV[2]
                 plainW[:,:,iIter] = CW[0]*X + CW[1]*Y + CW[2]
-            
-            
-        
-    
+
+
     elif eddyOrder == 2:
         # best-fit quadratic curve
         
         for iIter in range(plainU.shape[2]):
-            
-            eddyMaskTemp = eddyMask[:,:,iIter]
-            indEddyMask = numpy.argwhere(eddyMaskTemp.ravel()).ravel()
-            BU = UOrgMean[:, :, iIter].ravel()[indEddyMask]
-            BV = VOrgMean[:, :, iIter].ravel()[indEddyMask]
-            BW = WOrgMean[:, :, iIter].ravel()[indEddyMask]
-                    
-            D = numpy.ones((len(indEddyMask), 6))
-            D[:, 0] = X.ravel()[indEddyMask]
-            D[:, 1] = Y.ravel()[indEddyMask]
-            D[:, 2] = XY.ravel()[indEddyMask]
-            D[:, 3] = X2.ravel()[indEddyMask]
-            D[:, 4] = Y2.ravel()[indEddyMask]
-            
-       
-            CU,_,_,_ = scipy.linalg.lstsq(D, BU)    # coefficients
-            CV,_,_,_ = scipy.linalg.lstsq(D, BV)    # coefficients
-            CW,_,_,_ = scipy.linalg.lstsq(D, BW)
+
+            BUInd = staticTissueU[:,:,iIter].copy()
+            BVInd = staticTissueV[:,:,iIter].copy()
+            BWInd = staticTissueW[:,:,iIter].copy()
+
+            notZeroIndU = numpy.nonzero(BUInd)
+            notZeroIndV = numpy.nonzero(BVInd)
+            notZeroIndW = numpy.nonzero(BWInd)
+
+            BU = BUInd[notZeroIndU].ravel()
+            BV = BVInd[notZeroIndV].ravel()
+            BW = BWInd[notZeroIndW].ravel()
+                
+            DU = numpy.c_[X[notZeroIndU].ravel(), Y[notZeroIndU].ravel(), XY[notZeroIndU].ravel(), X2[notZeroIndU].ravel() , Y2[notZeroIndU].ravel() , numpy.ones(len(X[notZeroIndU].ravel()))]
+            DV = numpy.c_[X[notZeroIndV].ravel(), Y[notZeroIndV].ravel(), XY[notZeroIndV].ravel(), X2[notZeroIndV].ravel() , Y2[notZeroIndV].ravel() , numpy.ones(len(X[notZeroIndV].ravel()))]
+            DW = numpy.c_[X[notZeroIndW].ravel(), Y[notZeroIndW].ravel(), XY[notZeroIndW].ravel(), X2[notZeroIndW].ravel() , Y2[notZeroIndW].ravel() , numpy.ones(len(X[notZeroIndW].ravel()))]
+
+            CU,_,_,_ = scipy.linalg.lstsq(DU, BU)    # coefficients
+            CV,_,_,_ = scipy.linalg.lstsq(DV, BV)    # coefficients
+            CW,_,_,_ = scipy.linalg.lstsq(DW, BW)
         
             # evaluate it on grid
             plainU[:,:,iIter] = CU[0]*X + CU[1]*Y + CU[2]*XY + CU[3]*X2 + CU[4]*Y2 + CU[5]
@@ -158,25 +166,7 @@ def eddyCurrentCorrection(UOrg, VOrg, WOrg, randNoiseThreshold=1, eddyCurrentThr
     
     if plotEddyPlane:
         plt.figure(2)
-        #ax = fig.add_subplot(111, projection='3d')
-            
-        #for i in range(staticTissueU[20]):
-
         Axes3D.plot_surface(X, Y,  plainU[:,:,20])  
-
-            
-
-       
-#            plt.subplot(121)
- #           plt.imshow(UOrg[:,:,i,1], vmin=vmin, vmax=vmax, cmap='seismic')
-  #          plt.title('Org data')
-
-
-           
-   #         plt.subplot(122)
-    #        plt.imshow(UOrgtest[:,:,i], vmin=vmin, vmax=vmax, cmap='seismic')
-     #       plt.title('static tissue')
-
         plt.show()  
    
 
@@ -202,13 +192,7 @@ def randNoise(UOrg, VOrg, WOrg, randThre=25, plotBool=1):
         USTD[1:(UOrg.shape[0]-1),1:(UOrg.shape[1]-1), kIter] = numpy.std(rolling_window(UOrg[:,:,kIter,:], (3,3,0), toend=False), axis=(4,3,1))
         VSTD[1:(VOrg.shape[0]-1),1:(VOrg.shape[1]-1), kIter] = numpy.std(rolling_window(VOrg[:,:,kIter,:], (3,3,0), toend=False), axis=(4,3,1))
         WSTD[1:(WOrg.shape[0]-1),1:(WOrg.shape[1]-1), kIter] = numpy.std(rolling_window(WOrg[:,:,kIter,:], (3,3,0), toend=False), axis=(4,3,1))
-
-      
-   # UOrgtest = UOrg[:,:,:,1].copy()
-
-   # UOrgtest[(USTD > (randThre*USTD.max()/100)) & (VSTD > (randThre*VSTD.max()/100) ) & (WSTD > (randThre*WSTD.max()/100))] = 0
             
-
     
     flowCorrected = numpy.zeros([UOrg.shape[0], UOrg.shape[1], UOrg.shape[2],3,UOrg.shape[3]])
 
